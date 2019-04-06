83e109105d969981df0d255084d8ba7464b422d8
Markitanov Denis <dmarkitanov@gmail.com>
2019-04-06 11:23:42 +0600

    initial commit

---
+++ sync.c
@@ -0,0 +1,59 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/delay.h>
+#include <linux/spinlock.h>
+
+#define TAG "week9" 
+
+static struct task_struct *t1;
+static struct task_struct *t2;
+static struct task_struct *t3;
+static struct task_struct *t4;
+static struct task_struct *t5;
+
+DEFINE_SPINLOCK(lock);
+
+static int counter = 0;
+static int * arr[];
+int arr_size;
+
+static int threadMethod(void *unused) {
+    int i = 0;
+    int local_counter = counter;
+
+    for (i=0; i<5; i++) {
+        counter++;
+        spin_lock(&lock);
+        printk(KERN_INFO "I'm %s, counter: %d", current->comm, counter);
+        spin_unlock(&lock);
+        msleep(100);
+    }
+
+    return 0;
+}
+
+void createThreads(void) {
+    t1 = kthread_run(threadMethod, NULL, "mythread1");
+    t2 = kthread_run(threadMethod, NULL, "mythread2");
+    t3 = kthread_run(threadMethod, NULL, "mythread3");
+    t4 = kthread_run(threadMethod, NULL, "mythread4");
+    t5 = kthread_run(threadMethod, NULL, "mythread5");
+
+}
+
+int init_module(void)
+{
+    printk(KERN_INFO "START %s\n", TAG);
+    arr = generator();    
+    arr_size = sizeof(arr)/sizeof(arr[0]);
+
+    createThreads();
+    return 0;
+}
+
+void cleanup_module(void)
+{
+    printk(KERN_INFO "END %s\n", TAG);
+}
